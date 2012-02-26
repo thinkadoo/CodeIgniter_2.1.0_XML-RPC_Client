ef4f17a315da06ae910f897d7e0ded902002d1fa
Andre Venter <andre.n.venter@gmail.com>
2012-02-26 09:45:25 +0200

    change baseURL

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1/CodeIgniter_2.1.0_XML-RPC_Client/';
+//$config['base_url']	= 'http://127.0.0.1/CodeIgniter_2.1.0_XML-RPC_Client/';
+$config['base_url']	= 'http://localhost/CodeIgniter_2.1.0_XML-RPC_Client/';
 
 /*
 |--------------------------------------------------------------------------
